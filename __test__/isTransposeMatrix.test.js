974e585449c8c00ff0f537ec51f97ee1fbbec6d3
Matthew Alvarez <mattalvarez354@yahoo.com>
2023-09-27 14:55:28 -0400

    Adding jest test

---
+++ __test__/isTransposeMatrix.test.js
@@ -0,0 +1,40 @@
+const transposeMatrix = require("../transposeMatrix");
+
+describe("transposeMatrix", () => {
+  it("should return the transposed matrix", () => {
+    const matrix = [[1]];
+
+    expect(transposeMatrix(matrix)).toBe([[1]]);
+  });
+
+  it("should return the transposed matrix", () => {
+    const matrix = [[1, 2, 3]];
+
+    expect(transposeMatrix(matrix)).toBe([[1], [2], [3]]);
+  });
+
+  it("should return the transposed matrix", () => {
+    const matrix = [[1], [-1]];
+
+    expect(transposeMatrix(matrix)).toBe([[1, -1]]);
+  });
+
+  it("should return the transposed matrix", () => {
+    const matrix = [[1], [2], [3]];
+
+    expect(transposeMatrix(matrix)).toBe([[1, 2, 3]]);
+  });
+
+  it("should return the transposed matrix", () => {
+    const matrix = [
+      [1, 2, 3],
+      [4, 5, 6],
+    ];
+
+    expect(transposeMatrix(matrix)).toBe([
+      [1, 4],
+      [2, 5],
+      [3, 6],
+    ]);
+  });
+});
