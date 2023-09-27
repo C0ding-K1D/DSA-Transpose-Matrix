c26d38121a2a312dc1634953c78851ecf644cf3f
Matthew Alvarez <mattalvarez354@yahoo.com>
2023-09-27 14:58:42 -0400

    fixing syntax error in tests

--- __test__/isTransposeMatrix.test.js
+++ __test__/isTransposeMatrix.test.js
@@ -4,25 +4,25 @@ describe("transposeMatrix", () => {
   it("should return the transposed matrix", () => {
     const matrix = [[1]];
 
-    expect(transposeMatrix(matrix)).toBe([[1]]);
+    expect(transposeMatrix(matrix)).toStrictEqual([[1]]);
   });
 
   it("should return the transposed matrix", () => {
     const matrix = [[1, 2, 3]];
 
-    expect(transposeMatrix(matrix)).toBe([[1], [2], [3]]);
+    expect(transposeMatrix(matrix)).toStrictEqual([[1], [2], [3]]);
   });
 
   it("should return the transposed matrix", () => {
     const matrix = [[1], [-1]];
 
-    expect(transposeMatrix(matrix)).toBe([[1, -1]]);
+    expect(transposeMatrix(matrix)).toStrictEqual([[1, -1]]);
   });
 
   it("should return the transposed matrix", () => {
     const matrix = [[1], [2], [3]];
 
-    expect(transposeMatrix(matrix)).toBe([[1, 2, 3]]);
+    expect(transposeMatrix(matrix)).toStrictEqual([[1, 2, 3]]);
   });
 
   it("should return the transposed matrix", () => {
@@ -31,7 +31,7 @@ describe("transposeMatrix", () => {
       [4, 5, 6],
     ];
 
-    expect(transposeMatrix(matrix)).toBe([
+    expect(transposeMatrix(matrix)).toStrictEqual([
       [1, 4],
       [2, 5],
       [3, 6],
